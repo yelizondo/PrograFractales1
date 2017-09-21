cb20b563eaf7821f0e1736f684720f8765d19313
vito1231 <vic.nb.99@hotmail.com>
2017-09-21 13:47:23 -0600

    Sierpinski un poco defectuoso, Levy C perfecto

--- fractal.cpp
+++ fractal.cpp
@@ -12,10 +12,10 @@ QVector<QLineF> Fractal::generateGraphics(char pFractal, SimpleList<char> *pList
 {
     QVector<QLineF> res; // Lineas a dibujar
     QLineF line1,line2;
-    int inicioX=553;
-    int inicioY=344;
+    int inicioX=750;
+    int inicioY=450;
     int angulo=0;
-    line1.setLine(inicioX,inicioY,inicioX+3,inicioY);
+    line1.setLine(inicioX,inicioY,inicioX+8,inicioY);
 
     switch (pFractal)
     {
@@ -44,12 +44,55 @@ QVector<QLineF> Fractal::generateGraphics(char pFractal, SimpleList<char> *pList
             break;
 
         case 'L': // LevyC Curve
+        for (int i = 0; i < pList->length(); i++)
+        {
+            if (pList->getPos(i)->getValue() == 'I') {
+                angulo=angulo+45;
+                line1.setAngle(angulo);
+
+
+            }
+            else
+                if (pList->getPos(i)->getValue() == 'D') {
+                    angulo=angulo-45;
+                    line1.setAngle(angulo);
+
+
+                }
+                else{
+                    res.append(line1);
+                    line2.setLine(line1.p2().rx(),line1.p2().ry(),line1.p2().rx()+8,line1.p2().ry());
+                    line1=line2;
+                }
+
+        }
+
 
 
             break;
 
         case 'S': // Sierpinski Curve
+        res.append(line1);
+        line2.setLine(line1.p2().rx(),line1.p2().ry(),line1.p2().rx()+0.5,line1.p2().ry());
+        line1=line2;
+        for (int i = 0; i < pList->length(); i++)
+        {
+                if (pList->getPos(i)->getValue() == 'I') {
+                    angulo=angulo+60;
+                    line1.setAngle(angulo);
+                    res.append(line1);
 
+                }
+                else
+                    if (pList->getPos(i)->getValue() == 'D') {
+                        angulo=angulo-60;
+                        line1.setAngle(angulo);
+                        res.append(line1);
+
+                    }
+                line2.setLine(line1.p2().rx(),line1.p2().ry(),line1.p2().rx()+0.5,line1.p2().ry());
+                line1=line2;
+         }
 
             break;
 
